de03aefd49732b08b61997b914e8251c1d415c9f
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-10-10 23:59:26 +0200

    fix/api

--- backend/api/twitterApi.js
+++ backend/api/twitterApi.js
@@ -6,10 +6,12 @@ module.exports.twitterRequest = async (req, res, next) => {
   const { headerLink } = req.body;
   const response = await client.tweets
     .tweetsRecentSearch({
-      query: `${headerLink} has:images`,
+      query: `${headerLink} has:images -is:retweet`,
       max_results: 10,
+      sort_order: "relevancy",
+      "tweet.fields": ["attachments", "entities", "text"],
       expansions: ["attachments.media_keys"],
-      "media.fields": ["url"],
+      "media.fields": ["alt_text", "preview_image_url", "url"],
     })
     .then((data) => {
       res.status(200).send(data);
