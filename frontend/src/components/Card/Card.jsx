80a77078e1b98559314a30066ee9c2f9f65181fc
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-10-14 19:01:03 +0200

    fix: cards component

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./Card.css";
 
 export default function Card({ card, onCardClick, index }) {
-
-  console.log(index)
-
   function handleClick() {
     onCardClick(card);
   }
 
   return (
-    <div className={`card ${"area" + index}`}>
+    <div
+      className={`card ${"area" + index}`}
+      style={{ gridArea: `area${index}` }}
+    >
       <img
         src={card.url}
         className="card__image"
