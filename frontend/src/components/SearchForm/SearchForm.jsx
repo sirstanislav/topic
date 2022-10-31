e36a9cdbd0cb4f800a147aa80b664357aa5a8c1c
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-10-31 16:13:25 +0100

    fix: searchform component

--- frontend/src/components/SearchForm/SearchForm.jsx
+++ frontend/src/components/SearchForm/SearchForm.jsx
@@ -1,15 +1,30 @@
 import "./SearchForm.css";
 import React from "react";
+import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 
 export default function SearchForm() {
+  const { darkThemeState } = useSelector((state) => state.themeState);
+
+  const { register, handleSubmit, watch } = useForm(/*{ mode: "onblur" }*/);
+
   return (
-    <form className="searchform">
+    <form
+      className={
+        darkThemeState ? "searchform searchform_dark-theme" : "searchform"
+      }
+    >
       <input
-        className="searchform__input"
+        className={
+          darkThemeState
+            ? "searchform__input searchform__input_dark-theme"
+            : "searchform__input"
+        }
         type="text"
         placeholder="Поиск хештега"
         aria-label="Поиск хештега"
       ></input>
+      <button className="searchform__submit" type="submit"></button>
     </form>
   );
 }
